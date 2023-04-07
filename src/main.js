527b491b123b9780a0a00abc3175b0504a3383ac
tran <tranb1913275@student.ctu.edu.vn>
2023-04-07 23:06:01 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "@fortawesome/fontawesome-free/css/all.min.css";
+// import './assets/main.css'
+// import router from "./router";
+
+
+// createApp(App).mount('#app')
+
 import { createApp } from 'vue'
 import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-import './assets/main.css'
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "@fortawesome/fontawesome-free/css/all.min.css"
 
 import router from "./router";
 createApp(App).use(router).mount("#app");
